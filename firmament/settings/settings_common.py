02ab8f78fd377279f2d8c09d5d08ed0222288f78
Jack Cushman <jcushman@gmail.com>
2016-07-08 21:58:53 -0400

    Fixes.

--- firmament/settings/settings_common.py
+++ firmament/settings/settings_common.py
@@ -13,8 +13,8 @@ import os
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(PROJECT_ROOT)
 
 
 # Quick-start development settings - unsuitable for production
