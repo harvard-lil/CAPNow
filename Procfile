25d593e2ebe4d78a2c6e9120907bd3644d181038
Jack Cushman <jcushman@gmail.com>
2016-07-26 10:12:51 -0400

    Celery

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn firmament.wsgi --log-file -
+worker: celery -A firmament worker --loglevel=info -B
--- firmament/settings/settings_heroku.py
+++ firmament/settings/settings_heroku.py
@@ -13,3 +13,20 @@ WEBPACK_LOADER['DEFAULT'].update({
     'BUNDLE_DIR_NAME': 'dist/',
     'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats-prod.json'),
 })
+
+# message passing
+# settings via https://www.cloudamqp.com/docs/celery.html
+BROKER_POOL_LIMIT=1
+BROKER_URL = env('CLOUDAMQP_URL')
+BROKER_CONNECTION_TIMEOUT = 30
+BROKER_HEARTBEAT = 30
+CELERY_SEND_EVENTS = False  # on the free CloudAMQP plan, celery events rapidly eat up our monthly message quota
+CELERY_RESULT_BACKEND = env('REDIS_URL')
+CELERYD_HIJACK_ROOT_LOGGER = False
+
+# logging
+LOGGING['handlers']['default'] = {
+    'level': 'INFO',
+    'class': 'logging.StreamHandler',
+    'formatter': 'standard',
+}
