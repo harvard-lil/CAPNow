364fae36ebea59f9c630d2486663f9f66c282b98
Matt Phillips <mphillips@law.harvard.edu>
2017-02-06 14:39:11 -0500

    added a basic rss feed for cases. hopefully the cap db can consume this?

--- firmament/urls.py
+++ firmament/urls.py
@@ -18,14 +18,16 @@ from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
+from firmament.feeds import LatestEntriesFeed
+
 
 from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('api.urls', namespace='api')),
-
     url(r'^public$', views.public, name='public'),
+    url(r'^latest/feed/$', LatestEntriesFeed()),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home')
 ]
 
