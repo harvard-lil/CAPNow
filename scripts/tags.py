25b9a1c40012e2f4c7dedd4fad6b73ca6e82c13d
Anastasia <anastasia.aizman@gmail.com>
2017-02-08 14:13:59 -0500

    clean up

--- scripts/tags.py
+++ scripts/tags.py
@@ -4,6 +4,7 @@ class TagLibrary(dict):
     pass
 
 tag = TagLibrary()
+# html tags
 tag.h1 = tag.format_tag("h1")
 tag.h2 = tag.format_tag("h2")
 tag.h3 = tag.format_tag("h3")
@@ -11,10 +12,11 @@ tag.h4 = tag.format_tag("h4")
 tag.em = tag.format_tag("em")
 tag.p = tag.format_tag("p")
 tag.bold = tag.format_tag("bold")
+tag.sup = tag.format_tag("sup")
+# xml tags
 tag.party = tag.format_tag("party")
 tag.parties = tag.format_tag("parties")
 tag.name = tag.format_tag("name")
-tag.sup = tag.format_tag("sup")
 tag.decisiondate = tag.format_tag("decisiondate")
 tag.otherdate = tag.format_tag("otherdate")
 tag.judges = tag.format_tag("judges")
