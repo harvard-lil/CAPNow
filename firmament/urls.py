943b7fe45002eaad4b22efa7d2d6cdcd1dcad629
Ben Steinberg <ben@potswift.org>
2017-02-14 10:51:34 -0500

    More syntax correction

--- firmament/urls.py
+++ firmament/urls.py
@@ -32,9 +32,10 @@ urlpatterns = [
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^public$', views.public, name='public'),
     url(r'^latest/feed/$', CaseFeed()),
-    url(r'^$', login_required(TemplateView.as_view(template_name='index.html')), name='home')
+    url(r'^$', login_required(TemplateView.as_view(template_name='index.html')), name='home'),
     url(r'^public$', views.public, name='public'),
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='home')
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
